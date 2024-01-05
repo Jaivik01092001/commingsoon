f846f3696944bc1222a4d759db627871e1ad5b94
Patel Jaivik <120545678+Jaivik01092001@users.noreply.github.com>
2024-01-05 10:53:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="heroseccontainer">
+        <div className="herotop">
+          <div
+            initial={{ opacity: 0, y: -50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, ease: "easeOut" }}
+            viewport={{ once: true }}
+            className="herotitle"
+          >
+            Stunning HDR Photos <br /> using cutting-edge AI Technology, <br />
+            delivered Instantly
+          </div>
+          <div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, ease: "easeOut" }}
+            viewport={{ once: true }}
+            className="herodescription "
+          >
+            High-end digital experiences. Created at the heart of <br />{" "}
+            Manhattan, we are a human-sized team.
+          </div>
+          <h2 class="text-4xl lg:text-5xl text-white font-bold mb-8 commingsoon capitalize">
+            Our apps are coming soon...
+          </h2>
+          <p class="text-gray-400 text-lg lg:text-xl leading-relaxed mb-12 staytuned">
+            Stay tuned for updates and get ready for an extraordinary
+            experience!
+          </p>
+        </div>
+        <div className="herobottom">
+          {/* <img className="iphoneimg" src={iphone} alt="" /> */}
+          {/* <div className="mainherimgiphone flex">
+        <img
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 40 }}
+          transition={{ duration: 1, ease: "easeOut" }}
+          viewport={{ once: true }}
+          className="iphoneimgsize1"
+          src={firstiphone1}
+          alt=""
+        />
+        <img
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 25 }}
+          transition={{ duration: 1, ease: "easeOut" }}
+          viewport={{ once: true }}
+          className="iphoneimgsize2"
+          src={firstiphone2}
+          alt=""
+        />
+        <img
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 2, ease: "easeOut" }}
+          viewport={{ once: true }}
+          className="iphoneimgsize3"
+          src={firstiphone3}
+          alt=""
+        />
+        <img
+          initial={{ opacity: 0, x: 100 }}
+          whileInView={{ opacity: 1, x: -25 }}
+          transition={{ duration: 1, ease: "easeOut" }}
+          viewport={{ once: true }}
+          className="iphoneimgsize4"
+          src={firstiphone4}
+          alt=""
+        />
+        <img
+          initial={{ opacity: 0, x: 100 }}
+          whileInView={{ opacity: 1, x: -40 }}
+          transition={{ duration: 1, ease: "easeOut" }}
+          viewport={{ once: true }}
+          className="iphoneimgsize5"
+          src={firstiphone5}
+          alt=""
+        />
+      </div> */}
+        </div>
+      </div>
+    </>
   );
 }
 
